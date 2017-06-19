04487ee181d1f8f0ea51ffb266dc673b7cd96a99
laurenceberry <berry.laur@gmail.com>
2017-06-19 11:13:40 +0100

    Create basic flow

---
+++ app/routes.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var yaml = require('js-yaml');
+var express = require('express');
+var router = express.Router();
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+// add your routes here
+
+module.exports = router
+
+router.get('/award/:page/:yml', function (req, res) {
+  var templateData = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
+  );
+
+  Object.keys(req.query).map(function(key) {
+    templateData[key] = req.query[key];
+  });
+
+  templateData.storedValues = req.cookies;
+
+  console.log("--- GET", req.path);
+  console.log(JSON.stringify(templateData, null, '  '));
+
+  res.render(
+    "award/" + req.params.page,
+    templateData
+  );
+
+});
