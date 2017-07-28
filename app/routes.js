9788d449f398e56318afdb0451c8a643407d2f52
laurenceberry <berry.laur@gmail.com>
2017-07-28 14:54:38 +0100

    Set up shortlisting flow

--- app/routes.js
+++ app/routes.js
@@ -29,3 +29,21 @@ router.get('/award/:page/:yml', function (req, res) {
   );
 
 });
+
+router.get('/shortlist/:page/:yml', function (req, res) {
+  var templateData = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
+  );
+
+  Object.keys(req.query).map(function(key) {
+    templateData[key] = req.query[key];
+  });
+
+  templateData.storedValues = req.cookies;
+
+  res.render(
+    "shortlist/" + req.params.page,
+    templateData
+  );
+
+});
