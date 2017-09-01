59a2d0a4d8965a51b02e50e7c584e3ea9bf81094
laurenceberry <berry.laur@gmail.com>
2017-09-01 16:39:28 +0100

    Merge shortlist and award flows

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,25 @@ router.get('/', function (req, res) {
 
 module.exports = router
 
+router.post('/award/suppliers/award', function (req, res) {
+
+  var award_status = req.body.award_status
+
+  console.log('award_status: ' + award_status)
+
+  if (award_status == 'yes') {
+    // redirect to the relevant page
+    res.redirect('/award/suppliers/award')
+  } else if (award_status == 'no') {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/award/no_details/award?awarding=true')
+  } else {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/award/overview/award')
+  }
+})
+
+
 router.get('/award/:page/:yml', function (req, res) {
   var templateData = yaml.safeLoad(
     fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
@@ -25,6 +44,63 @@ router.get('/award/:page/:yml', function (req, res) {
 
   res.render(
     "award/" + req.params.page,
+    templateData
+  );
+
+});
+
+
+router.post('/shortlist/suppliers/award', function (req, res) {
+
+  var shortlisting_status = req.body.shortlisting_status
+
+  console.log('shortlisting_status: ' + shortlisting_status)
+
+  if (shortlisting_status == 'yes') {
+    // redirect to the relevant page
+    res.redirect('/shortlist/suppliers/award')
+  } else if (shortlisting_status == 'no') {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/shortlist/no_details/award')
+  } else {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/award/overview/award')
+  }
+})
+
+
+
+router.get('/shortlist/:page/:yml', function (req, res) {
+  var templateData = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
+  );
+
+  Object.keys(req.query).map(function(key) {
+    templateData[key] = req.query[key];
+  });
+
+  templateData.storedValues = req.cookies;
+
+  res.render(
+    "shortlist/" + req.params.page,
+    templateData
+  );
+
+});
+
+router.get('/supplier/:page/:yml', function (req, res) {
+  var templateData = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
+  );
+
+  Object.keys(req.query).map(function(key) {
+    templateData[key] = req.query[key];
+  });
+
+  templateData.storedValues = req.cookies;
+
+  res.render(
+    "supplier/" + req.params.page,
     templateData
   );
 
